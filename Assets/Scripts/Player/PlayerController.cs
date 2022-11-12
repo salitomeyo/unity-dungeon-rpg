e65bbdb7ae301bfac4af6f878a277ef0afdcf82a
Salome Aristizabal Giraldo <salitomeyo16@gmail.com>
2022-11-12 17:22:58 -0500

    feat: player controller

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private CharacterController controller;
+    [SerializeField] private float movementSpeed = 6.0f;
+    [SerializeField] private float rotationSpeed = 240.0f;
+
+    private void Start()
     {
-        
+        controller = gameObject.AddComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        PlayerMovement();
+        PlayerRotation();
+    }
+
+    void PlayerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontalInput, verticalInput, 0);
+        controller.Move(move * Time.deltaTime * movementSpeed);
+    }
+
+    void PlayerRotation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+
+        float angle = Mathf.Atan2(mousePos.x, mousePos.y) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, -angle);
     }
 }
